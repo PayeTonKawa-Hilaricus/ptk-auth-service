f989e2cdf692f7b2f92c349b5753bac0666eeef9
Hilaricus Stephane <stephane_hilaricus@ens.univ-artois.fr>
2025-12-29 16:38:24 +0100

    test(auth): correction des mocks et atteinte des 100% de couverture

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Ajout recommandé
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  // --- CONFIGURATION SWAGGER ---
+  const config = new DocumentBuilder()
+    .setTitle('Auth Service API')
+    .setDescription("Micro-service d'authentification (JWT)")
+    .setVersion('1.0')
+    .addTag('auth')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  // -----------------------------
+
+  await app.listen(3000);
 }
 bootstrap();
